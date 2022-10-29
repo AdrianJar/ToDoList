0caf66ccfe6392beb777581d6ff3b7edc04376b4
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2022-10-29 17:09:11 +0200

    Adding a task and added done text decoration

--- script.js
+++ script.js
@@ -15,19 +15,46 @@
 
         for (const task of tasks) {
             htmlString += `
-                <li class="${task.done ? " list__taskDone" : ""}">
+                <li 
+                class="${task.done ? " list__taskDone" : ""}"
+                >
                     ${task.content}
                 </li>
             `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const onFromSubmit = (event) => {
+        event.preventDefault();
+
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
     }
-
-
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFromSubmit);
+
     };
+
 
     init();
 }
