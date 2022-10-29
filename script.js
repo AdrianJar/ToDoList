67d389c800efa3e6f2c927277d9ac80e62a17309
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2022-10-29 16:26:20 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+{
+    const tasks = [
+        {
+            content: "Zrobić pracę domową",
+            done: false,
+        },
+        {
+            content: "Zjeść obiad",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li>
+                    ${task.content}
+                </li>
+            `;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    }
+
+
+
+    const init = () => {
+        render();
+    };
+
+    init();
+}
